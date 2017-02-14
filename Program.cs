f70bfcaf2c7ba7d4f1fd6dba55cfdbc36a1bf816
Ryan Brandenburg <rybrande@microsoft.com>
2017-02-13 16:53:35 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Resources;
+using System.Globalization;
+using System.Reflection;
+
+namespace ResourceManagerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resourceManager = new ResourceManager(
+                "ResourceManagerTest.Resources.Program",
+                typeof(Program).GetTypeInfo().Assembly
+            );
+
+            var french = new CultureInfo("fr-FR");
+
+            CultureInfo.CurrentCulture = french;
+            CultureInfo.CurrentUICulture = french;
+
+            Console.WriteLine(resourceManager.GetString("Hello"));
+        }
+    }
+}
